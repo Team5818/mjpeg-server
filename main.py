15b5b0588846ddb245c05007579b41942e3d1152
Kenzie Togami <ket1999@gmail.com>
2017-02-17 16:40:40 -0800

    Slightly better perf

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3.4
 from multiprocessing import Manager
 
 from server import CaptureHTTPServer
@@ -9,7 +10,7 @@ PORT = 9090
 def main():
     manager = Manager()
     comms, evt = start_vision_process(manager)
-    CaptureHTTPServer(('', PORT), comms, evt).serve_forever(poll_interval=0)
+    CaptureHTTPServer(('', PORT), comms, evt).serve_forever(poll_interval=0.015)
 
 
 if __name__ == '__main__':
